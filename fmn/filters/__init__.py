baf0fae3e0c8b502273e474af1c6e5866f6a41f5
Ralph Bean <rbean@redhat.com>
2013-11-18 11:12:11 -0500

    Add a tautology filter.

--- fmn/filters/__init__.py
+++ fmn/filters/__init__.py
@@ -2,3 +2,13 @@ from fmn.filters.ansible import *
 from fmn.filters.askbot import *
 from fmn.filters.bodhi import *
 from fmn.filters.pkgdb import *
+
+
+def DevelopmentFilter(*args, **kwargs):
+    """ All messages
+
+    This lets every message through not matter the content.
+
+    (Useful for development)
+    """
+    return True
