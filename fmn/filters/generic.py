c02d6bef8a91d436e9c26363f810eb8053ca2a69
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-11-19 18:08:04 +0100

    Fix the user_filter to handle arguments properly
    fasnick=None is required so that the argument is displayed in the UI
    *args, **kw is actually how the arguments are passed to the filter for it to run
    This **kw is the actual important bit here.

--- fmn/filters/generic.py
+++ fmn/filters/generic.py
@@ -1,11 +1,13 @@
 # Generic filters for FMN
+import fedmsg
 
-def user_filter(config, message, fasnick):
+def user_filter(config, message, fasnick=None, *args, **kw):
     """ Filters the messages by the user that performed the action.
 
     Use this filter to filter out messages that are associated with a
     specified user.
     """
-    print config
 
-    return fasnick in fedmsg.meta.msg2usernames(message)
+    fasnick = kw.get('fasnick', fasnick)
+    if fasnick:
+        return fasnick in fedmsg.meta.msg2usernames(message)
