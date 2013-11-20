7bc4afdde415ec4176c589fb867ccdee2db5c041
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-11-20 14:18:20 +0100

    Add first filter relying on pkgdb integration

--- fmn/filters/generic.py
+++ fmn/filters/generic.py
@@ -1,6 +1,8 @@
 # Generic filters for FMN
 import fedmsg
 
+import fmn.lib.pkgdb
+
 
 def user_filter(config, message, fasnick=None, *args, **kw):
     """ All messages of user
@@ -12,3 +14,16 @@ def user_filter(config, message, fasnick=None, *args, **kw):
     fasnick = kw.get('fasnick', fasnick)
     if fasnick:
         return fasnick in fedmsg.meta.msg2usernames(message)
+
+
+def user_package_filter(config, message, fasnick=None, *args, **kw):
+    """ All messages concerning user's packages
+
+    This filter filters out messages that related to packages where the
+    specified user has **commit** ACLs.
+    """
+
+    fasnick = kw.get('fasnick', fasnick)
+    if fasnick:
+        packages = fmn.lib.pkgdb.get_package_of_user(fasnick)
+        return packages.intersection(fedmsg.meta.msg2packages(message))
