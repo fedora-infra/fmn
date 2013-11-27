8a1b5a86d28a15a4469e9a4b47d96e37c569901c
Ralph Bean <rbean@redhat.com>
2013-11-27 14:13:58 -0500

    FAS docstrings.

--- fmn/rules/fas.py
+++ fmn/rules/fas.py
@@ -1,62 +1,78 @@
 def fas_group_create(config, message):
-    """ Fas: New group created.
+    """ FAS: New group created
 
-    TODO description for the web interface goes here
+    Adding this rule to a filter will allow through `Fedora Account System
+    <https://admin.fedoraproject.org/accounts>`_ notifications indicating that
+    a **new group has been created**.
     """
     return message['topic'].endswith('fas.group.create')
 
 
 def fas_group_member_apply(config, message):
-    """ Fas: A member requested to join a group.
+    """ FAS: A member requested to join a group
 
-    TODO description for the web interface goes here
+    Adding this rule to a filter will allow through `Fedora Account System
+    <https://admin.fedoraproject.org/accounts>`_ notifications indicating that
+    a **user has requested to join a group**.
     """
     return message['topic'].endswith('fas.group.member.apply')
 
 
 def fas_group_member_remove(config, message):
-    """ Fas: A user was removed from a group.
+    """ FAS: A user was removed from a group
 
-    TODO description for the web interface goes here
+    Adding this rule to a filter will allow through `Fedora Account System
+    <https://admin.fedoraproject.org/accounts>`_ notifications indicating that
+    a **user was removed from a group**.
     """
     return message['topic'].endswith('fas.group.member.remove')
 
 
 def fas_group_member_sponsor(config, message):
-    """ Fas: A user has been sponsored by an authorized user into a group.
+    """ FAS: A user has been sponsored by an authorized user into a group
 
-    TODO description for the web interface goes here
+    Adding this rule to a filter will allow through `Fedora Account System
+    <https://admin.fedoraproject.org/accounts>`_ notifications indicating that
+    a **user has been sponsored into a group**.
     """
     return message['topic'].endswith('fas.group.member.sponsor')
 
 
 def fas_group_update(config, message):
-    """ Fas: A group's properties have been modified.
+    """ FAS: A group's properties have been modified
 
-    TODO description for the web interface goes here
+    Adding this rule to a filter will allow through `Fedora Account System
+    <https://admin.fedoraproject.org/accounts>`_ notifications indicating that
+    a **group's details were updated**.
     """
     return message['topic'].endswith('fas.group.update')
 
 
 def fas_role_update(config, message):
-    """ A user's role in a particular group has been updated.
+    """ FAS: A user's role in a particular group has been updated
 
-    TODO description for the web interface goes here
+    Adding this rule to a filter will allow through `Fedora Account System
+    <https://admin.fedoraproject.org/accounts>`_ notifications indicating that
+    a **user's role in a group was updated**.
     """
     return message['topic'].endswith('fas.role.update')
 
 
 def fas_user_create(config, message):
-    """ Fas: A new user account has been created.
+    """ FAS: A new user account has been created
 
-    TODO description for the web interface goes here
+    Adding this rule to a filter will allow through `Fedora Account System
+    <https://admin.fedoraproject.org/accounts>`_ notifications indicating that
+    a **new user was created**.
     """
     return message['topic'].endswith('fas.user.create')
 
 
 def fas_user_update(config, message):
-    """ Fas: A user updated his/her account.
+    """ FAS: A user updated his/her account
 
-    TODO description for the web interface goes here
+    Adding this rule to a filter will allow through `Fedora Account System
+    <https://admin.fedoraproject.org/accounts>`_ notifications indicating that
+    a **user updated their account information**.
     """
     return message['topic'].endswith('fas.user.update')
