794234559d4009311aaddec46a995057f97f4969
Ralph Bean <rbean@redhat.com>
2013-11-27 13:19:22 -0500

    releng docstrings.

--- fmn/rules/compose.py
+++ fmn/rules/compose.py
@@ -1,109 +1,177 @@
 def compose_branched_complete(config, message):
-    """ Compose: Compose completed for a specific branch
+    """ Release Engineering: Compose completed for a specific branch
 
-    TODO description for the web interface goes here
+    Adding this rule will allow through notifications published when `release
+    engineering <https://fedoraproject.org/wiki/ReleaseEngineering>`_ completes
+    the **entire** `compose <https://apps.fedoraproject.org/releng-dash/>`_ of
+    the 'branched' Fedora pre-release.
     """
     return message['topic'].endswith('compose.branched.complete')
 
 
 def compose_branched_mash_complete(config, message):
-    """ Compose: Mash completed for a specific branch
+    """ Release Engineering: Mash completed for a specific branch
 
-    TODO description for the web interface goes here
+    Adding this rule will allow through notifications published when `release
+    engineering <https://fedoraproject.org/wiki/ReleaseEngineering>`_ completes
+    the **mash** phase of a `compose
+    <https://apps.fedoraproject.org/releng-dash/>`_ of the 'branched' Fedora
+    pre-release.
     """
     return message['topic'].endswith('compose.branched.mash.complete')
 
 
 def compose_branched_mash_start(config, message):
-    """ Compose: Mash started for a specific branch
+    """ Release Engineering: Mash started for a specific branch
 
-    TODO description for the web interface goes here
+    Adding this rule will allow through notifications published when `release
+    engineering <https://fedoraproject.org/wiki/ReleaseEngineering>`_ starts
+    the **mash** phase of a `compose
+    <https://apps.fedoraproject.org/releng-dash/>`_ of the 'branched' Fedora
+    pre-release.
     """
     return message['topic'].endswith('compose.branched.mash.start')
 
 
 def compose_branched_pungify_complete(config, message):
-    """ Compose: Pungi completed for a specific branch
+    """ Release Engineering: Pungi completed for a specific branch
 
-    TODO description for the web interface goes here
+    Adding this rule will allow through notifications published when `release
+    engineering <https://fedoraproject.org/wiki/ReleaseEngineering>`_ completes
+    the **pungi** phase of a `compose
+    <https://apps.fedoraproject.org/releng-dash/>`_ of the 'branched' Fedora
+    pre-release.
     """
     return message['topic'].endswith('compose.branched.pungify.complete')
 
 
 def compose_branched_pungify_start(config, message):
-    """ Compose: Pungi started for a specific branch
+    """ Release Engineering: Pungi started for a specific branch
 
-    TODO description for the web interface goes here
+    Adding this rule will allow through notifications published when `release
+    engineering <https://fedoraproject.org/wiki/ReleaseEngineering>`_ starts
+    the **pungi** phase of a `compose
+    <https://apps.fedoraproject.org/releng-dash/>`_ of the 'branched' Fedora
+    pre-release.
     """
     return message['topic'].endswith('compose.branched.pungify.start')
 
 
 def compose_branched_rsync_complete(config, message):
-    """ Compose: rsync completed for a specific branch
+    """ Release Engineering: rsync completed for a specific branch
 
-    TODO description for the web interface goes here
+    Adding this rule will allow through notifications published when `release
+    engineering <https://fedoraproject.org/wiki/ReleaseEngineering>`_ finishes
+    **rsyncing** a `compose <https://apps.fedoraproject.org/releng-dash/>`_ of
+    the 'branched' Fedora pre-release.
     """
     return message['topic'].endswith('compose.branched.rsync.complete')
 
 
 def compose_branched_rsync_start(config, message):
-    """ Compose: rsync started for a specific branch
+    """ Release Engineering: rsync started for a specific branch
 
-    TODO description for the web interface goes here
+    Adding this rule will allow through notifications published when `release
+    engineering <https://fedoraproject.org/wiki/ReleaseEngineering>`_ starts
+    **rsyncing** a `compose <https://apps.fedoraproject.org/releng-dash/>`_ of
+    the 'branched' Fedora pre-release.
     """
     return message['topic'].endswith('compose.branched.rsync.start')
 
 
 def compose_branched_start(config, message):
-    """ Compose: Compose started for a specific branch
+    """ Release Engineering: Compose started for a specific branch
 
-    TODO description for the web interface goes here
+    Adding this rule will allow through notifications published when `release
+    engineering <https://fedoraproject.org/wiki/ReleaseEngineering>`_ starts
+    the `compose <https://apps.fedoraproject.org/releng-dash/>`_ process for
+    the 'branched' Fedora pre-release.
     """
     return message['topic'].endswith('compose.branched.start')
 
-
 def compose_rawhide_complete(config, message):
-    """ Compose: Compose completed for rawhide
+    """ Release Engineering: Compose completed for rawhide
 
-    TODO description for the web interface goes here
+    Adding this rule will allow through notifications published when `release
+    engineering <https://fedoraproject.org/wiki/ReleaseEngineering>`_ completes
+    the **entire** `compose <https://apps.fedoraproject.org/releng-dash/>`_ of
+    rawhide.
     """
     return message['topic'].endswith('compose.rawhide.complete')
 
 
 def compose_rawhide_mash_complete(config, message):
-    """ Compose: Mash completed for rawhide
+    """ Release Engineering: Mash completed for rawhide
 
-    TODO description for the web interface goes here
+    Adding this rule will allow through notifications published when `release
+    engineering <https://fedoraproject.org/wiki/ReleaseEngineering>`_ completes
+    the **mash** phase of a `compose
+    <https://apps.fedoraproject.org/releng-dash/>`_ of rawhide.
     """
     return message['topic'].endswith('compose.rawhide.mash.complete')
 
-def compose_rawhide_mash_start(config, message):
-    """ Compose: Mash started for rawhide
 
-    TODO description for the web interface goes here
+def compose_rawhide_mash_start(config, message):
+    """ Release Engineering: Mash started for rawhide
+
+    Adding this rule will allow through notifications published when `release
+    engineering <https://fedoraproject.org/wiki/ReleaseEngineering>`_ starts
+    the **mash** phase of a `compose
+    <https://apps.fedoraproject.org/releng-dash/>`_ of rawhide.
     """
     return message['topic'].endswith('compose.rawhide.mash.start')
 
 
-def compose_rawhide_rsync_complete(config, message):
-    """ Compose:
+def compose_rawhide_pungify_complete(config, message):
+    """ Release Engineering: Pungi completed for rawhide
 
-    TODO description for the web interface goes here
+    Adding this rule will allow through notifications published when `release
+    engineering <https://fedoraproject.org/wiki/ReleaseEngineering>`_ completes
+    the **pungi** phase of a `compose
+    <https://apps.fedoraproject.org/releng-dash/>`_ of rawhide.
+    """
+    return message['topic'].endswith('compose.rawhide.pungify.complete')
+
+
+def compose_rawhide_pungify_start(config, message):
+    """ Release Engineering: Pungi started for rawhide
+
+    Adding this rule will allow through notifications published when `release
+    engineering <https://fedoraproject.org/wiki/ReleaseEngineering>`_ starts
+    the **pungi** phase of a `compose
+    <https://apps.fedoraproject.org/releng-dash/>`_ of rawhide.
+    """
+    return message['topic'].endswith('compose.rawhide.pungify.start')
+
+
+def compose_rawhide_rsync_complete(config, message):
+    """ Release Engineering: rsync completed for rawhide
+
+    Adding this rule will allow through notifications published when `release
+    engineering <https://fedoraproject.org/wiki/ReleaseEngineering>`_ finishes
+    **rsyncing** a `compose <https://apps.fedoraproject.org/releng-dash/>`_ of
+    rawhide.
     """
     return message['topic'].endswith('compose.rawhide.rsync.complete')
 
 
 def compose_rawhide_rsync_start(config, message):
-    """ Compose: rsync started for rawhide
+    """ Release Engineering: rsync started for rawhide
 
-    TODO description for the web interface goes here
+    Adding this rule will allow through notifications published when `release
+    engineering <https://fedoraproject.org/wiki/ReleaseEngineering>`_ starts
+    **rsyncing** a `compose <https://apps.fedoraproject.org/releng-dash/>`_ of
+    rawhide.
     """
     return message['topic'].endswith('compose.rawhide.rsync.start')
 
 
 def compose_rawhide_start(config, message):
-    """ Compose: Compose started for rawhide
+    """ Release Engineering: Compose started for rawhide
 
-    TODO description for the web interface goes here
+    Adding this rule will allow through notifications published when `release
+    engineering <https://fedoraproject.org/wiki/ReleaseEngineering>`_ starts
+    the `compose <https://apps.fedoraproject.org/releng-dash/>`_ process for
+    rawhide.
     """
     return message['topic'].endswith('compose.rawhide.start')
