f26a0f779f1cdf9e5883bb76055969e5ded0125b
Ralph Bean <rbean@redhat.com>
2013-11-27 14:51:43 -0500

    mailman docstrings.

--- fmn/rules/mailman.py
+++ fmn/rules/mailman.py
@@ -1,6 +1,10 @@
 def mailman_receive(config, message):
     """ Mailman: An email has been sent to a mailing list
 
-    TODO description for the web interface goes here
+    Including this rule will trigger a notification anytime
+    an **email is posted** to any Fedora Project **mailman3** list.
+
+    If you're using this to send yourself emails, it might make more sense to
+    just *subscribe to the list*... think about it.
     """
     return message['topic'].endswith('mailman.receive')
