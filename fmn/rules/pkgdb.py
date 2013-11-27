f4e0345e8e284a302d0c8ab8c2095d5f1374b7fc
Ralph Bean <rbean@redhat.com>
2013-11-27 15:13:47 -0500

    pkgdb docstrings.

--- fmn/rules/pkgdb.py
+++ fmn/rules/pkgdb.py
@@ -1,7 +1,9 @@
 def pkgdb_acl_update(config, message):
     """ Pkgdb: a user updated an ACL
 
-    TODO description for the web interface goes here
+    Adding this rule will trigger notifications when an ACL on a package
+    is **updated** in the Fedora `Package DB
+    <https://admin.fedoraproject.org/pkgdb>`_.
     """
     return message['topic'].endswith('pkgdb.acl.update')
 
@@ -9,7 +11,9 @@ def pkgdb_acl_update(config, message):
 def pkgdb_acl_user_remove(config, message):
     """ Pkgdb: a user removed an ACL
 
-    TODO description for the web interface goes here
+    Adding this rule will trigger notifications when an ACL on a package
+    is **removed** from the Fedora `Package DB
+    <https://admin.fedoraproject.org/pkgdb>`_.
     """
     return message['topic'].endswith('pkgdb.acl.user.remove')
 
@@ -17,7 +21,9 @@ def pkgdb_acl_user_remove(config, message):
 def pkgdb_branch_clone(config, message):
     """ Pkgdb: branched a specific package
 
-    TODO description for the web interface goes here
+    Adding this rule will trigger notifications when a new branch is cloned
+    for a package in the Fedora `Package DB
+    <https://admin.fedoraproject.org/pkgdb>`_.
     """
     return message['topic'].endswith('pkgdb.branch.clone')
 
@@ -25,7 +31,9 @@ def pkgdb_branch_clone(config, message):
 def pkgdb_branch_complete(config, message):
     """ Pkgdb: finished the branching process
 
-    TODO description for the web interface goes here
+    Adding this rule will trigger notifications when the **branching process
+    completes** for a package in the Fedora `Package DB
+    <https://admin.fedoraproject.org/pkgdb>`_.
     """
     return message['topic'].endswith('pkgdb.branch.complete')
 
@@ -33,7 +41,9 @@ def pkgdb_branch_complete(config, message):
 def pkgdb_branch_start(config, message):
     """ Pkgdb: started the branching process
 
-    TODO description for the web interface goes here
+    Adding this rule will trigger notifications when the **branching process
+    starts** for a package in the Fedora `Package DB
+    <https://admin.fedoraproject.org/pkgdb>`_.
     """
     return message['topic'].endswith('pkgdb.branch.start')
 
@@ -41,7 +51,9 @@ def pkgdb_branch_start(config, message):
 def pkgdb_collection_new(config, message):
     """ Pkgdb: new collection created
 
-    TODO description for the web interface goes here
+    Adding this rule will trigger notifications when an admin **creates
+    a new collection** in the Fedora `Package DB
+    <https://admin.fedoraproject.org/pkgdb>`_.
     """
     return message['topic'].endswith('pkgdb.collection.new')
 
@@ -49,7 +61,9 @@ def pkgdb_collection_new(config, message):
 def pkgdb_collection_update(config, message):
     """ Pkgdb: a collection has been updated
 
-    TODO description for the web interface goes here
+    Adding this rule will trigger notifications when an admin **updates
+    a collection** in the Fedora `Package DB
+    <https://admin.fedoraproject.org/pkgdb>`_.
     """
     return message['topic'].endswith('pkgdb.collection.update')
 
@@ -57,7 +71,10 @@ def pkgdb_collection_update(config, message):
 def pkgdb_critpath_update(config, message):
     """ Pkgdb: a user updated a critpath status
 
-    TODO description for the web interface goes here
+    Adding this rule will trigger notifications when **the critical path
+    status** of a package in the Fedora `Package DB
+    <https://admin.fedoraproject.org/pkgdb>`_ **changes** (when a package is
+    either added or removed from the critical path).
     """
     return message['topic'].endswith('pkgdb.critpath.update')
 
@@ -65,7 +82,9 @@ def pkgdb_critpath_update(config, message):
 def pkgdb_owner_update(config, message):
     """ Pkgdb: a user updated the owner of a package
 
-    TODO description for the web interface goes here
+    Adding this rule will trigger notifications when the **owner** of a package
+    in the Fedora `Package DB <https://admin.fedoraproject.org/pkgdb>`_
+    **changes**.  This includes when a package is orphaned.
     """
     return message['topic'].endswith('pkgdb.owner.update')
 
@@ -73,7 +92,11 @@ def pkgdb_owner_update(config, message):
 def pkgdb_package_new(config, message):
     """ Pkgdb: a new package has been created
 
-    TODO description for the web interface goes here
+    Adding this rule will trigger notifications when a **new package is
+    created** in the Fedora `Package DB
+    <https://admin.fedoraproject.org/pkgdb>`_.  This typically happens near the
+    end of the Package Review Process as a result of a `SCM Admin Request
+    <http://fedoraproject.org/wiki/Package_SCM_admin_requests>`_.
     """
     return message['topic'].endswith('pkgdb.package.new')
 
@@ -81,7 +104,9 @@ def pkgdb_package_new(config, message):
 def pkgdb_package_retire(config, message):
     """ Pkgdb: a package has been retired
 
-    TODO description for the web interface goes here
+    Adding this rule will trigger notifications when a **package is
+    retired** from the Fedora `Package DB
+    <https://admin.fedoraproject.org/pkgdb>`_.
     """
     return message['topic'].endswith('pkgdb.package.retire')
 
@@ -89,6 +114,8 @@ def pkgdb_package_retire(config, message):
 def pkgdb_package_update(config, message):
     """ Pkgdb: a user updated information about a package
 
-    TODO description for the web interface goes here
+    Adding this rule will trigger notifications when a **package's details are
+    updated** in the Fedora `Package DB
+    <https://admin.fedoraproject.org/pkgdb>`_.
     """
     return message['topic'].endswith('pkgdb.package.update')
