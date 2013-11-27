3c15a2e78256df0d358845508c537b42580e83cd
Ralph Bean <rbean@redhat.com>
2013-11-27 14:58:39 -0500

    Meetbot messages.

--- fmn/rules/meetbot.py
+++ fmn/rules/meetbot.py
@@ -1,12 +1,9 @@
 def meetbot_meeting_complete(config, message):
     """ Meetbot: Meeting completed
 
-Trusty old `zodbot <https://meetbot.fedoraproject.org/>`_ publishes
-messages too!  Messages on this topic get published when an IRC meeting
-ends.  Meetings may or may not have a title (which can be tricky).
-Here's an example message where the title is specified:
-
-    TODO description for the web interface goes here
+    Trusty old `zodbot <https://meetbot.fedoraproject.org/>`_ publishes
+    messages too!  Adding this rule will notify you when an IRC meeting
+    ends.
     """
     return message['topic'].endswith('meetbot.meeting.complete')
 
@@ -14,24 +11,18 @@ Here's an example message where the title is specified:
 def meetbot_meeting_start(config, message):
     """ Meetbot: Meeting started
 
-Trusty old `zodbot <https://meetbot.fedoraproject.org/>`_ publishes
-messages too!  Messages on this topic get published (somewhat obviously)
-when a new IRC meeting is started.  The user starting the meeting may
-specify a meeting title, but doesn't have to.  Here's an example
-message with a specified meeting title:
-
-    TODO description for the web interface goes here
+    Trusty old `zodbot <https://meetbot.fedoraproject.org/>`_ publishes
+    messages too!  Adding this rule will notify you (perhaps obviously)
+    when a new IRC meeting is started.
     """
     return message['topic'].endswith('meetbot.meeting.start')
-
 
 
 def meetbot_meeting_topic_update(config, message):
     """ Meetbot: Topic of a meeting changed
 
-As IRC meetings chug along, the chairperson may change the meeting;
-zodbot publishes message for that!  An example **with** a title specified:
-
-    TODO description for the web interface goes here
+    As IRC meetings chug along, the chairperson may change the meeting;
+    zodbot publishes message for that!  Guess what?  Adding this rule will let
+    you get notifications about that.
     """
     return message['topic'].endswith('meetbot.meeting.topic.update')
