274ed1707a1c439bbc14a066bfb8a2f0c01182f5
Ralph Bean <rbean@redhat.com>
2013-11-27 15:26:03 -0500

    wiki docstrings.

--- fmn/rules/wiki.py
+++ fmn/rules/wiki.py
@@ -1,8 +1,9 @@
 def wiki_article_edit(config, message):
     """ Wiki: A user edited a wiki page
 
-Fedora's `Wiki <https://fedoraproject.org/wiki>`_ has a fedmsg hook
-that publishes messages like this one when a user edits a page.
+    Fedora's `Wiki <https://fedoraproject.org/wiki>`_ has a fedmsg hook
+    that publishes messages when a user **edits a page**.  Adding this rule
+    will trigger notifications whenever that event occurs.
     """
     return message['topic'].endswith('wiki.article.edit')
 
@@ -10,7 +11,9 @@ that publishes messages like this one when a user edits a page.
 def wiki_upload_complete(config, message):
     """ Wiki: A user uploaded content on the wiki
 
-Fedora's `Wiki <https://fedoraproject.org/wiki>`_ hook also publishes
-messages when a user upload some media (like a video or a picture).
+    Fedora's `Wiki <https://fedoraproject.org/wiki>`_ has a fedmsg hook
+    that publishes messages when a user **uploads some media** (like a video or
+    a picture).  Adding this rule will trigger notifications whenever that
+    event occurs.
     """
     return message['topic'].endswith('wiki.upload.complete')
