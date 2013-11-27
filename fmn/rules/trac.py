4c71d923f2fccd966e5560c9ca35a558698ddf40
Ralph Bean <rbean@redhat.com>
2013-11-27 15:24:09 -0500

    Fedora hosted docstrings.

--- fmn/rules/trac.py
+++ fmn/rules/trac.py
@@ -1,70 +1,80 @@
 def trac_git_receive(config, message):
-    """ Trac: A user pushed a changed to the git repository of the trac.
+    """ Fedora Hosted: Commits pushed to a git repository
 
-    TODO description for the web interface goes here
+    Adding this rule will get you notifications when a user **pushes commits**
+    to a `fedorahosted <https://fedorahosted.org>`_ git repository.
     """
     return message['topic'].endswith('trac.git.receive')
 
 
 def trac_ticket_delete(config, message):
-    """ Trac: A user deleted a ticket on the trac
+    """ Fedora Hosted: Deleted a ticket on a trac instance
 
-    TODO description for the web interface goes here
+    Adding this rule will get you notifications when a user **deletes a
+    ticket** from a `fedorahosted <https://fedorahosted.org>`_ trac instance.
     """
     return message['topic'].endswith('trac.ticket.delete')
 
 
 def trac_ticket_new(config, message):
-    """ Trac: A user created a new ticket on the trac
+    """ Fedora Hosted: Created a new ticket on a trac instance
 
-    TODO description for the web interface goes here
+    Adding this rule will get you notifications when a user **creates a
+    ticket** on a `fedorahosted <https://fedorahosted.org>`_ trac instance.
     """
     return message['topic'].endswith('trac.ticket.new')
 
 
 def trac_ticket_update(config, message):
-    """ Trac: A user updated a ticket on the trac
+    """ Fedora Hosted: Updated a ticket on a trac instance
 
-    TODO description for the web interface goes here
+    Adding this rule will get you notifications when a user **updates a
+    ticket** on a `fedorahosted <https://fedorahosted.org>`_ trac instance.
     """
     return message['topic'].endswith('trac.ticket.update')
 
 
 def trac_wiki_page_delete(config, message):
-    """ Trac: A user deleted a wiki page of the trac
+    """ Fedora Hosted: Deleted a wiki page of a trac instance
 
-    TODO description for the web interface goes here
+    Adding this rule will get you notifications when a user **deletes a wiki
+    page** on a `fedorahosted <https://fedorahosted.org>`_ trac instance.
     """
     return message['topic'].endswith('trac.wiki.page.delete')
 
 
 def trac_wiki_page_new(config, message):
-    """ Trac: A user created a wiki page of the trac
+    """ Fedora Hosted: Created a wiki page of a trac instance
 
-    TODO description for the web interface goes here
+    Adding this rule will get you notifications when a user **creates a wiki
+    page** on a `fedorahosted <https://fedorahosted.org>`_ trac instance.
     """
     return message['topic'].endswith('trac.wiki.page.new')
 
 
 def trac_wiki_page_rename(config, message):
-    """ Trac: A user renamed a wiki page of the trac
+    """ Fedora Hosted: Renamed a wiki page of a trac instance
 
-    TODO description for the web interface goes here
+    Adding this rule will get you notifications when a user **renames a wiki
+    page** on a `fedorahosted <https://fedorahosted.org>`_ trac instance.
     """
     return message['topic'].endswith('trac.wiki.page.rename')
 
 
 def trac_wiki_page_update(config, message):
-    """ Trac: A user updated a wiki page of the trac
+    """ Fedora Hosted: Updated a wiki page of a trac instance
 
-    TODO description for the web interface goes here
+    Adding this rule will get you notifications when a user **updates a wiki
+    page** on a `fedorahosted <https://fedorahosted.org>`_ trac instance.
     """
     return message['topic'].endswith('trac.wiki.page.update')
 
 
 def trac_wiki_page_version_delete(config, message):
-    """ Trac: A user deleted a version of a wiki page of the trac
+    """ Fedora Hosted: Deleted a version of a wiki page
 
-    TODO description for the web interface goes here
+    Adding this rule will get you notifications when a user **deletes a version
+    of a wiki page** on a `fedorahosted <https://fedorahosted.org>`_ trac
+    instance.
     """
     return message['topic'].endswith('trac.wiki.page.version.delete')
