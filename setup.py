abce2ac49cad35081c0c04c74022af4b04bac317
Ralph Bean <rbean@redhat.com>
2013-12-03 14:43:05 -0500

    0.1.0

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ tests_require = [
 
 setup(
     name='fmn.lib',
-    version='0.0.1',
+    version='0.1.0',
     description='Internal API components and model for Fedora Notifications',
     long_description=get_description(),
     author='Ralph Bean',
