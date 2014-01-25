de2c03ba5782adf14ee3a804bef29e19c70f3225
Ricky Elrod <ricky@elrod.me>
2014-01-25 06:10:42 -0500

    Some prep work for Android

--- fmn/consumer/backends/android.py
+++ fmn/consumer/backends/android.py
@@ -12,6 +12,26 @@ class GCMBackend(BaseBackend):
         self.post_url = self.config['fmn.gcm.post_url']
         self.api_key = self.config['fmn.gcm.api_key']
 
+    def _send_notification(self, registration_id, data):
+      '''Immediately send a notification to a device. This does **NOT** check
+         whether or not the user has notifications enabled. The calling method
+         **MUST** do this itself.'''
+
+      headers = {
+          'Authorization': 'key=%s' % self.api_key,
+          'content-type': 'application/json',
+      }
+      body = {
+          'registration_ids': [registration_id],
+          'data': data,
+      }
+      response = requests.post(
+          self.post_url,
+          data=json.dumps(body),
+          headers=headers)
+      self.log.debug(" * got %r %r" % (response.status_code, response.text))
+
+
     def handle(self, recipient, msg):
         self.log.debug("Notifying via gcm/android %r" % recipient)
 
@@ -23,22 +43,23 @@ class GCMBackend(BaseBackend):
             self.log.debug("Messages stopped for %r, not sending." % nickname)
             return
 
-        headers = {
-            'Authorization': 'key=%s' % self.api_key,
-            'content-type': 'application/json',
-        }
-        body = {
-            'registration_ids': [recipient['registration id']],
-            'data': msg,
-        }
-        response = requests.post(
-            self.post_url,
-            data=json.dumps(body),
-            headers=headers)
-        self.log.debug(" * got %r %r" % (response.status_code, reponse.text))
+        self._send_notification(recipient['registration id'], msg)
 
     def handle_batch(self, recipient, queued_messages):
         raise NotImplementedError()
 
     def handle_confirmation(self, confirmation):
-        raise NotImplementedError()
+        confirmation.set_status(self.session, 'valid')
+        acceptance_url = self.config['fmn.acceptance_url'].format(
+            secret=confirmation.secret)
+        rejection_url = self.config['fmn.rejection_url'].format(
+            secret=confirmation.secret)
+
+        confirmation_msg = {
+            "title": "Fedora Notifications Confirmation"
+          , "message": "Hi! Tap this notification to confirm that you would" +
+                       " like to receive Fedora related notifications."
+          , "secret": confirmation.secret
+          }
+
+        self._send_notification(confirmation.detail_value, confirmation_msg)
