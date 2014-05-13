247980d9dedfa7278affd181da4a0df59436122d
Ralph Bean <rbean@redhat.com>
2014-05-13 11:42:53 -0400

    Mock out a notify method on the models for the tests.

--- fmn/lib/tests/__init__.py
+++ fmn/lib/tests/__init__.py
@@ -15,6 +15,14 @@ class Base(object):
         self.valid_paths = fmn.lib.load_rules(
             root='fmn.lib.tests.example_rules')
 
+        def mock_notify(self, openid, context, changed):
+            if not hasattr(self, 'notified'):
+                self.notified = []
+            self.notified.append([openid, context, changed])
+
+        self.original_notify = fmn.lib.models.FMNBase.notify
+        fmn.lib.models.FMNBase.notify = mock_notify
+
     def tearDown(self):
         """ Remove the test.db database if there is one. """
         dbfile = DB_PATH.split('///')[1]
@@ -22,3 +30,5 @@ class Base(object):
             os.unlink(dbfile)
 
         self.sess.rollback()
+
+        fmn.lib.models.FMNBase.notify = self.original_notify
