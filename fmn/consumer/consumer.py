c8069b98b1b5adb3a90b1feaa1512a09c64f06c6
Ralph Bean <rbean@redhat.com>
2014-09-04 14:18:13 -0700

    pep8

--- fmn/consumer/consumer.py
+++ fmn/consumer/consumer.py
@@ -111,10 +111,11 @@ class FMNConsumer(fedmsg.consumers.FedmsgConsumer):
             if group == 'packager':
                 usernames = fedmsg.meta.msg2usernames(msg, **self.hub.config)
                 for username in usernames:
+                    openid_url = 'https://%s.id.fedoraproject.org' % username
                     user = fmn.lib.models.User.get_or_create(
                         session,
                         openid='%s.id.fedoraproject.org' % username,
-                        openid_url='https://%s.id.fedoraproject.org' % username,
+                        openid_url=openid_url,
                         create_defaults=True,
                     )
                     session.add(user)
