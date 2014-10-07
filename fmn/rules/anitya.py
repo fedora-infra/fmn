f409289c75a3ff63d8f4d18ffc4be912011d7979
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-10-07 16:30:40 +0200

    Add the first rules for anitya integration in FMN

---
+++ fmn/rules/anitya.py
@@ -0,0 +1,82 @@
+def anitya_mapping_new(config, message):
+    """ Anitya: a new mapping of a project to a distribution has been added
+
+    Adding this rule will trigger notifications when a new mapping of a
+    project to a distribution is **added** in `anitya
+    <release-monitoring.org>`_.
+    """
+    return message['topic'].endswith('anitya.project.map.new')
+
+
+def anitya_mapping_update(config, message):
+    """ Anitya: the mapping of a project to a distribution has been updated
+
+    Adding this rule will trigger notifications when the mapping of a
+    project to a distribution is **updated** in `anitya
+    <release-monitoring.org>`_.
+    """
+    return message['topic'].endswith('anitya.project.map.update')
+
+
+def anitya_mapping_deleted(config, message):
+    """ Anitya: a mapping of a project to a distribution has been deleted
+
+    Adding this rule will trigger notifications when the mapping of a
+    project in a distribution is **deleted** in `anitya
+    <release-monitoring.org>`_.
+    """
+    return message['topic'].endswith('anitya.project.map.remove')
+
+
+def anitya_distro_add(config, message):
+    """ Anitya: a distribution is added
+
+    Adding this rule will trigger notifications when a new distribution is
+    **added** to `anitya <release-monitoring.org>`_.
+    """
+    return message['topic'].endswith('anitya.distro.add')
+
+
+def anitya_distro_update(config, message):
+    """ Anitya: a distribution has been updated
+
+    Adding this rule will trigger notifications when a distribution is
+    **updated** in `anitya <release-monitoring.org>`_.
+    """
+    return message['topic'].endswith('anitya.distro.edit')
+
+
+def anitya_project_add(config, message):
+    """ Anitya: a project is added
+
+    Adding this rule will trigger notifications when a project is **added**
+    to `anitya <release-monitoring.org>`_.
+    """
+    return message['topic'].endswith('anitya.project.add')
+
+
+def anitya_info_update(config, message):
+    """ Anitya: a project has been updated
+
+    Adding this rule will trigger notifications when the information about
+    a project are **updated** in `anitya <release-monitoring.org>`_.
+    """
+    return message['topic'].endswith('anitya.project.update')
+
+
+def anitya_project_deleted(config, message):
+    """ Anitya: a project is deleted
+
+    Adding this rule will trigger notifications when a project is **deleted**
+    to `anitya <release-monitoring.org>`_.
+    """
+    return message['topic'].endswith('anitya.project.remove')
+
+
+def anitya_new_update(config, message):
+    """ Anitya: a project has an update
+
+    Adding this rule will trigger notifications when a project has a
+    **new release** according to `anitya <release-monitoring.org>`_.
+    """
+    return message['topic'].endswith('anitya.project.remove')
