68be5aa807d314f29ad89bd6b8740a715cb17634
Ralph Bean <rbean@redhat.com>
2015-01-07 14:43:29 -0500

    Typofix.

--- fmn/lib/models.py
+++ fmn/lib/models.py
@@ -644,7 +644,7 @@ class Preference(BASE):
             if filter.name == filter_name:
                 return filter
 
-        raise ValueError("No such filter %r" % filter_id)
+        raise ValueError("No such filter %r" % filter_name)
 
     def has_filter(self, session, filter_id):
         for filter in self.filters:
