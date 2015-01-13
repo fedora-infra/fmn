f9bb4df31377b6c0c69f39d915ef7ae6ad836d8a
Ralph Bean <rbean@redhat.com>
2015-01-13 14:01:14 -0500

    Get and test all three: argspec, docstring, and custom attrs.

--- fmn/lib/tests/test_hinting.py
+++ fmn/lib/tests/test_hinting.py
@@ -1,13 +1,15 @@
-from nose.tools import eq_, assert_not_equals
+from nose.tools import eq_
 
 import fmn.lib.tests
 
 
-class TestDefaults(fmn.lib.tests.Base):
+class TestHintDecoration(fmn.lib.tests.Base):
     def test_hint_decoration(self):
         rules = self.valid_paths['fmn.lib.tests.example_rules']
         rule = rules['hint_masked_rule']
-        import pprint;
-        pprint.pprint(rule)
+
         eq_(rule['title'], u'This is a docstring.')
-        eq_(len(rule['args']), 1)
+
+        eq_(len(rule['args']), 3)
+
+        eq_(rule['datanommer-hints'], {'categories': ['whatever']})
