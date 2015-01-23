50068bee5f43f1612c316862e87da6ecb5b56286
Ralph Bean <rbean@redhat.com>
2015-01-23 13:46:41 -0500

    0.4.4

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ tests_require = [
 
 setup(
     name='fmn.rules',
-    version='0.4.3',
+    version='0.4.4',
     description='Message processing rules for Fedora Notifications',
     long_description=get_description(),
     author='Ralph Bean',
