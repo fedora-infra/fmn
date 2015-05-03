832b0d704145f6d32c4863e638231b47e4ae46ae
Ralph Bean <rbean@redhat.com>
2015-05-03 17:26:03 -0400

    0.6.0

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ tests_require = [
 
 setup(
     name='fmn.consumer',
-    version='0.5.2',
+    version='0.6.0',
     description='Backend worker daemon for Fedora Notifications',
     long_description=get_description(),
     author='Ralph Bean',
