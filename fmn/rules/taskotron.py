412303f54d6bd29a44d08cd2c0702b3de7123d89
Martin Krizek <mkrizek@redhat.com>
2016-01-20 12:35:39 +0100

    Add more taskotron rules

--- fmn/rules/taskotron.py
+++ fmn/rules/taskotron.py
@@ -10,7 +10,8 @@ def taskotron_result_new(config, message):
     """
     return message['topic'].endswith('taskotron.result.new')
 
-@hint(categories=['taskotron'], invertible=False)
+
+@hint(categories=['taskotron'])
 def taskotron_task(config, message, task=None):
     """ Particular taskotron task
 
@@ -24,5 +25,81 @@ def taskotron_task(config, message, task=None):
     if not task:
         return False
 
-    tasks = [item.strip() for item in task.split(',')]
-    return message['msg']['task'].get('name') in tasks
+    tasks = [item.strip().lower() for item in task.split(',')]
+    return message['msg']['task'].get('name').lower() in tasks
+
+
+@hint(categories=['taskotron'])
+def taskotron_changed_outcome(config, message):
+    """ Taskotron task outcome changed
+
+    With this rule, you can limit messages to only those task results
+    with changed outcomes. This is useful when an object (a build,
+    an update, etc) gets retested and either the object itself or the
+    environment changes and the task outcome is now different (e.g.
+    FAILED -> PASSED).
+    """
+
+    outcome = message['msg']['result'].get('outcome')
+    prev_outcome = message['msg']['result'].get('prev_outcome')
+
+    return outcome != prev_outcome
+
+
+@hint(categories=['taskotron'])
+def taskotron_task_outcome(config, message, outcome=None):
+    """ Particular taskotron task outcome
+
+    With this rule, you can limit messages to only those of particular
+    `taskotron <https://taskotron.fedoraproject.org/>`_ task outcome.
+
+    You can specify several outcomes by separating them with a comma ',',
+    i.e.: ``PASSED,FAILED``.
+
+    The full list of supported outcomes can be found in the libtaskotron
+    `documentation <https://docs.qadevel.cloud.fedoraproject.org/
+    libtaskotron/latest/resultyaml.html#minimal-version>`_.
+    """
+
+    if not outcome:
+        return False
+
+    outcomes = [item.strip().lower() for item in outcome.split(',')]
+    return message['msg']['result'].get('outcome').lower() in outcomes
+
+
+@hint(categories=['taskotron'])
+def taskotron_task_failed_or_changed_outcome(config, message):
+    """ Taskotron task failed or changed outcome
+
+    With this rule, you can limit messages to only those task results
+    with FAILED outcome or those with changed outcomes. This rule is
+    a handy way of filtering a very useful use case - being notified
+    when either check failed for an item (a build, an update, etc),
+    or the item was fixed and the check now passes (i.e. changed
+    outcome).
+    """
+
+    outcome = message['msg']['result'].get('outcome')
+    prev_outcome = message['msg']['result'].get('prev_outcome')
+
+    return outcome == 'FAILED' or outcome != prev_outcome
+
+
+@hint(categories=['taskotron'])
+def taskotron_release_critical_task(config, message):
+    """ Release-critical taskotron tasks
+
+    With this rule, you can limit messages to only those of
+    release-critical
+    `taskotron <https://taskotron.fedoraproject.org/>`_ task.
+
+    These are the checks which are deemed extremely important
+    by the distribution, and their failure should be carefully
+    inspected. Currently these checks are ``depcheck`` and
+    ``upgradepath``.
+    """
+
+    task = message['msg']['task'].get('name')
+
+    return task in ['depcheck', 'rpmlint']
