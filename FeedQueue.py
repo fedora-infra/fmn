3a3159d5ae98b85b359240a3e497affb1716742e
skrzepto <shims506@gmail.com>
2016-06-30 09:30:00 -0500

    seperated the web-server and the rabbitmq interface

---
+++ FeedQueue.py
@@ -0,0 +1,43 @@
+import pika
+
+
+class FeedQueue:
+    def __init__(self, host='localhost',
+                 queue_name='skrzepto.id.fedoraproject.org',
+                 expire_ms=60000):
+
+        self.host = host
+        self.queue_name = queue_name
+        self.expire_ms = expire_ms
+
+        self.channel, self.connection = self._get_pika_channel_connection()
+
+    def receive_one_message(self):
+        # modified but src is below
+        # src: http://stackoverflow.com/questions/9876227/rabbitmq-consume-one-message-if-exists-and-quit
+
+        if self.connection.is_closed:
+            self.channel, self.connection = self._get_pika_channel_connection()
+
+        method_frame, header_frame, body = self.channel.basic_get(queue=self.queue_name)
+        if not method_frame:
+            self.connection.close()
+            return ''
+
+        if method_frame.NAME == 'Basic.GetEmpty':
+            self.connection.close()
+            return ''
+        else:
+            self.channel.basic_ack(delivery_tag=method_frame.delivery_tag)
+            self.connection.close()
+            # print body
+            return body
+
+    def _get_pika_channel_connection(self):
+        """ Connect to pika server and return channel and connection"""
+        parameters = pika.ConnectionParameters(host=self.host)
+        connection = pika.BlockingConnection(parameters)
+        channel = connection.channel()
+        channel.queue_declare(queue=self.queue_name, durable=True,
+                              arguments={'x-message-ttl': self.expire_ms, })
+        return channel, connection
--- sse-webserver.py
+++ sse-webserver.py
@@ -1,12 +1,9 @@
 import json
 import urllib2
-
-import pika
-from twisted.web.resource import Resource
-from twisted.web import server
-from twisted.internet import reactor
-from twisted.python.util import println
 from twisted.internet import interfaces, reactor, defer, task
+from FeedQueue import FeedQueue
+from twisted.web import server, resource
+from twisted.internet import reactor
 
 
 def get_recent_posts():
@@ -18,44 +15,6 @@ def get_recent_posts():
     json_response = json.loads(contents)
     return json_response['raw_messages']
 
-
-def receive_one_message(host='localhost',
-                        queue_name='skrzepto.id.fedoraproject.org',
-                        expire_ms=60000):
-    # modified but src is below
-    # src: http://stackoverflow.com/questions/9876227/rabbitmq-consume-one-message-if-exists-and-quit
-    channel, connection = _get_pika_channel(host=host,
-                                            queue_name=queue_name,
-                                            expire_ms=expire_ms)
-    method_frame, header_frame, body = channel.basic_get(queue=queue_name)
-    if not method_frame:
-        connection.close()
-        return ''
-
-    if method_frame.NAME == 'Basic.GetEmpty':
-        connection.close()
-        return ''
-    else:
-        channel.basic_ack(delivery_tag=method_frame.delivery_tag)
-        connection.close()
-        #print body
-        return body
-
-
-def _get_pika_channel(host='localhost',
-                      queue_name='skrzpeto.id.fedoraproject.org',
-                      expire_ms=60000):
-    """ Connect to pika server and return channel and connection"""
-    parameters = pika.ConnectionParameters(host=host)
-    connection = pika.BlockingConnection(parameters)
-    channel = connection.channel()
-    channel.queue_declare(queue=queue_name, durable=True,
-                          arguments={'x-message-ttl': expire_ms, })
-    return channel, connection
-
-
-from twisted.web import server, resource
-from twisted.internet import reactor
 
 class Simple(resource.Resource):
     isLeaf = True
@@ -72,7 +31,8 @@ class Simple(resource.Resource):
         return server.NOT_DONE_YET
 
     def write_messages(self, request):
-        data = receive_one_message(expire_ms=24 * 60 * 60 * 1000)
+        fq = FeedQueue()
+        data = fq.receive_one_message()
         request.write(data)
 
 site = server.Site(Simple())
