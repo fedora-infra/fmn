2ff3c926b6fd184af837d9c40a198dcdb89f005d
skrzepto <shims506@gmail.com>
2016-07-12 16:16:37 -0500

    removing unecessary exhange config and adding comment to expiration

--- fedmsg.d/sse.py
+++ fedmsg.d/sse.py
@@ -3,8 +3,7 @@ config = {
     # SSE
     "fmn.sse.pika.host": "localhost",
     "fmn.sse.pika.port": 5672,
-    "fmn.sse.pika.exchange": "",
-    "fmn.sse.pika.msg_expiration": 3600000,
+    "fmn.sse.pika.msg_expiration": 3600000,  # 1 hour in ms
 
     # SSE WebServer
     "fmn.sse.webserver.tcp_port": 8080,
