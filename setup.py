f1763d4e09b169edb06409ed3704cb3162c35d16
Pierre-Yves Chibon <pingou@pingoured.fr>
2016-07-15 16:43:17 +0200

    Release 0.9.0

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ tests_require = [
 
 setup(
     name='fmn.rules',
-    version='0.8.2',
+    version='0.9.0',
     description='Message processing rules for Fedora Notifications',
     long_description=get_description(),
     author='Ralph Bean',
