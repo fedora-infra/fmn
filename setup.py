34e4a920039adb9dcf825770d1f36237a3c4fe71
Jeremy Cline <jeremy@jcline.org>
2016-10-10 12:46:23 -0400

    Declare supported Python versions in setup.py
    This adds classifiers to the setup.py for the versions of Python this
    project supports.
    
    Signed-off-by: Jeremy Cline <jeremy@jcline.org>

--- setup.py
+++ setup.py
@@ -54,6 +54,12 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Intended Audience :: Developers',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     cmdclass={'test': PyTest},
 )
