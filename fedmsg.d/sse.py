3d42e9a744ced9b86b06563f5152d0c9410dc997
Jeremy Cline <jeremy@jcline.org>
2016-11-09 09:00:19 -0500

    Add additional configuration options for the SSE server
    This adds a blacklist and whitelist for queue names that the SSE server
    will serve. It also lets the QoS options for the queue consumer be
    configured and the access control header to be set.
    
    fixes #57
    
    Signed-off-by: Jeremy Cline <jeremy@jcline.org>

--- fedmsg.d/sse.py
+++ fedmsg.d/sse.py
@@ -9,6 +9,24 @@ config = {
     "fmn.sse.webserver.tcp_port": 8080,
     "fmn.sse.webserver.log": "sse.log",
 
+    # A regular expression using the standard Python re syntax that defines a
+    # whitelist of queues exposed by the SSE server.
+    'fmn.sse.webserver.queue_whitelist': '.+\.id\.fedoraproject\.org$',
+
+    # A regular expression using the standard Python re syntax that defines a
+    # blacklist for queues exposed by the SSE server. Any queue name that is
+    # matched by the regular expression will return a HTTP 403 to the client.
+    #
+    # Note: This is applied _after_ the whitelist so if the queue is matched
+    # by both regular expressions, the queue _will not_ be served.
+    'fmn.sse.webserver.queue_blacklist': None,
+
+    # The value to use with the 'Access-Control-Allow-Origin' HTTP header
+    'fmn.sse.webserver.allow_origin': '*',
+
+    # Define how many messages to prefetch from the AMQP server
+    'fmn.sse.pika.prefetch_count': 5,
+
     'endpoints': {
         # Just need this entry here for tests to pass on travis-ci.
     }
