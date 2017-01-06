6f98bdf60f180b77580dad83cfe34fcb7f8a7bac
Jeremy Cline <jeremy@jcline.org>
2017-01-06 16:03:07 -0500

    Update the license to be GPLv2+
    fixes #66
    
    Signed-off-by: Jeremy Cline <jeremy@jcline.org>

--- fmn/sse/server.py
+++ fmn/sse/server.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-
+# Copyright (C) 2017 Jeremy Cline
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
@@ -12,7 +13,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+# USA.
 
 from __future__ import absolute_import, unicode_literals
 
