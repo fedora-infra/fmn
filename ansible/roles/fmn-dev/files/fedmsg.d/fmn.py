491090c6d83b47ee3456274b84a095ee65ab24fe
Ryan Lerch <rlerch@redhat.com>
2022-04-12 14:29:28 +1000

    remove datanommer from vagrant and use postgres
    remove setting up datanommer in the vagrant setup (we disable it by
    default now) and switch the postgres groundwork we were using for
    datanommer to use it for the main fmn database (rather than using sqlite)
    
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- ansible/roles/fmn-dev/files/fedmsg.d/fmn.py
+++ ansible/roles/fmn-dev/files/fedmsg.d/fmn.py
@@ -5,9 +5,6 @@ hostname = socket.gethostname().split('.')[0]
 config = {
     # Consumer stuff
     "fmn.consumer.enabled": True,
-
-    # Our web frontend also needs to be able to talk to datanommer to get
-    # example messages that match rules (optional)
-    "datanommer.sqlalchemy.url": "postgresql+psycopg2://datanommer@localhost:5432/datanommer",
-    'datanommer.enabled': True,
+    "datanommer.enabled": False,
+    "fmn.sqlalchemy.uri":  "postgresql://postgres:anypasswordworkslocally@localhost/notifications",
 }
