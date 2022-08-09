0547ae4aa2998f8e1bd5a1aec3bb423605dd9bb8
Ryan Lerch <rlerch@redhat.com>
2022-08-09 18:46:50 +1000

    Run eslint over the frontend code
    used `npm run lint` which ran
    `eslint . --ext .vue,.js,.jsx,.cjs,.mjs,.ts,.tsx,.cts,.mts --fix --ignore-path .gitignore`
    
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- frontend/vite.vagrant.config.ts
+++ frontend/vite.vagrant.config.ts
@@ -1,25 +1,25 @@
-import { fileURLToPath, URL } from 'url'
-import * as fs from 'fs';
+import { fileURLToPath, URL } from "url";
+import * as fs from "fs";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
   clearScreen: false,
   server: {
     watch: {
-      usePolling: true
+      usePolling: true,
     },
     https: {
-      key: fs.readFileSync('/etc/pki/tls/private/server.key'),
-      cert: fs.readFileSync('/etc/pki/tls/certs/server.pem')
-    }
-  }
-})
+      key: fs.readFileSync("/etc/pki/tls/private/server.key"),
+      cert: fs.readFileSync("/etc/pki/tls/certs/server.pem"),
+    },
+  },
+});
