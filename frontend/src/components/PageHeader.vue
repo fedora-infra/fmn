6c850d58df4613420d7cde26ada40373509000e9
Ryan Lerch <rlerch@redhat.com>
2022-08-15 14:19:31 +1000

    FE: Migrate PageHeader.vue to use coreui components
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- frontend/src/components/PageHeader.vue
+++ frontend/src/components/PageHeader.vue
@@ -1,28 +1,31 @@
 <script setup lang="ts">
+import {
+  CNavbar,
+  CContainer,
+  CNavbarBrand,
+  CNavbarNav,
+} from "@coreui/bootstrap-vue";
 import LoginButton from "./LoginButton.vue";
 import MainNavLink from "./MainNavLink.vue";
 </script>
 
 <template>
-  <header>
-    <nav
-      class="navbar navbar-expand-lg navbar-light masthead justify-content-between py-2"
-    >
-      <div class="container">
-        <a class="navbar-brand" href="/"
-          ><img
-            src="@/assets/logo.png"
-            alt="Fedora Account System logo"
-            height="40"
-        /></a>
-        <ul class="navbar-nav fw-bold align-items-center">
-          <MainNavLink to="/">Home</MainNavLink>
-          <MainNavLink to="/about">About</MainNavLink>
-          <MainNavLink to="/rules">Rules</MainNavLink>
-          <MainNavLink to="/destinations">Destinations</MainNavLink>
-          <LoginButton />
-        </ul>
-      </div>
-    </nav>
-  </header>
+  <CNavbar
+    expand="lg"
+    color-scheme="light"
+    class="masthead justify-content-between py-2"
+  >
+    <CContainer>
+      <CNavbarBrand href="/">
+        <img src="@/assets/logo.png" alt="FMN logo" height="40" />
+      </CNavbarBrand>
+      <CNavbarNav class="align-items-center">
+        <MainNavLink to="/">Home</MainNavLink>
+        <MainNavLink to="/about">About</MainNavLink>
+        <MainNavLink to="/rules">Rules</MainNavLink>
+        <MainNavLink to="/destinations">Destinations</MainNavLink>
+        <LoginButton />
+      </CNavbarNav>
+    </CContainer>
+  </CNavbar>
 </template>
