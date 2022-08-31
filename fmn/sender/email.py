87da33c2f0efefa7522baed4444775e19f8ccc35
Aurélien Bompard <aurelien@bompard.org>
2022-08-31 18:22:09 +0200

    Make the sender async
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- fmn/sender/email.py
+++ fmn/sender/email.py
@@ -1,23 +1,28 @@
-import smtplib
+import logging
 from email.message import EmailMessage
+
+import aiosmtplib
 
 from .handler import Handler
 
+_log = logging.getLogger(__name__)
+
 
 class EmailHandler(Handler):
-    def setup(self):
-        self._smtp = smtplib.SMTP(
-            host=self._config.get("smtp_host", "localhost"), port=self._config("smtp_port", 25)
+    async def setup(self):
+        self._smtp = aiosmtplib.SMTP(
+            self._config.get("smtp_host", "localhost"), self._config.get("smtp_port", 25)
         )
+        await self._smtp.connect()
 
-    def stop(self):
-        self._smtp.quit()
+    async def stop(self):
+        await self._smtp.quit()
 
-    def handle(self, message):
+    async def handle(self, message):
         notif = EmailMessage()
         notif["From"] = self._config["from"]
         for name, value in message["headers"].items():
             notif[name] = value
         notif.set_content(message["body"])
-
-        self._smtp.send_message(notif)
+        _log.debug(f"Sending email to {notif['To']} with subject {notif['Subject']}")
+        await self._smtp.send_message(notif)
--- fmn/sender/handler.py
+++ fmn/sender/handler.py
@@ -1,30 +1,18 @@
-import json
-
-
 class Handler:
     def __init__(self, config):
         self._config = config
 
-    def setup(self):
+    async def setup(self):
         # Here we connect to the destination server if relevant.
         pass
 
-    def stop(self):
+    async def stop(self):
         pass
 
-    def on_message(self, channel, method_frame, header_frame, body):
-        try:
-            self.handle(json.loads(body))
-        except Exception:
-            channel.basic_nack(delivery_tag=method_frame.delivery_tag)
-            raise
-        else:
-            channel.basic_ack(delivery_tag=method_frame.delivery_tag)
-
-    def handle(self, message):
+    async def handle(self, message):
         raise NotImplementedError
 
 
 class PrintHandler(Handler):
-    def handle(self, message):
+    async def handle(self, message):
         print("Received:", message)
