a7967e87743d445b1c5d3286a1e712b23d2670ea
Aurélien Bompard <aurelien@bompard.org>
2022-08-31 18:22:33 +0200

    Sender: add an IRC handler
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

---
+++ fmn/sender/irc.py
@@ -0,0 +1,54 @@
+import asyncio
+import logging
+from urllib.parse import urlparse
+
+import irc.client
+import irc.client_aio
+import irc.connection
+
+from .handler import Handler
+
+_log = logging.getLogger(__name__)
+
+
+class IRCHandler(Handler):
+    async def setup(self):
+        self._client = IRCClient()
+        irc_url = urlparse(self._config["irc_url"])
+        await self._client.connect(
+            irc_url.hostname,
+            irc_url.port,
+            irc_url.username,
+            password=irc_url.password,
+            connect_factory=irc.connection.AioFactory(ssl=(irc_url.scheme == "ircs")),
+        )
+        _log.debug("IRC connection established")
+
+    async def stop(self):
+        _log.debug("Stopping IRC handler...")
+        await self._client.disconnect()
+
+    async def handle(self, message):
+        _log.debug(f"Trying to send {message}")
+        await self._client.privmsg(message["to"], message["message"])
+
+
+class IRCClient(irc.client_aio.AioSimpleIRCClient):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._connection_future = None
+
+    async def connect(self, *args, **kwargs):
+        self._connection_future = asyncio.Future()
+        await self.connection.connect(*args, **kwargs)
+        await self._connection_future
+
+    async def privmsg(self, *args, **kwargs):
+        # This is not async yet.
+        return self.connection.privmsg(*args, **kwargs)
+
+    def on_welcome(self, connection, event):
+        self._connection_future.set_result(connection)
+
+    async def disconnect(self):
+        return self.connection.disconnect()
