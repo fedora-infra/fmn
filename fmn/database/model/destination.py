8f0e0d9d59d7698f8c3ab961e233098f1d595dc7
Aurélien Bompard <aurelien@bompard.org>
2022-09-13 12:29:09 +0200

    Add models
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

---
+++ fmn/database/model/destination.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, UnicodeText
+from sqlalchemy.orm import relationship
+
+from ..main import Base
+from .generation_rule import GenerationRule
+
+
+class Destination(Base):
+    __tablename__ = "destinations"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+
+    generation_rule_id = Column(Integer, ForeignKey(GenerationRule.id), nullable=False)
+    generation_rule = relationship(GenerationRule, backref="destinations")
+
+    protocol = Column(String(length=255), nullable=False)
+    address = Column(UnicodeText, nullable=False)
---
+++ fmn/database/model/tracking_rule.py
@@ -0,0 +1,18 @@
+from sqlalchemy import JSON, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import backref, relationship
+
+from ..main import Base
+from .rule import Rule
+
+
+class TrackingRule(Base):
+    __tablename__ = "tracking_rules"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+
+    # Unique: there can be only one TrackingRule per Rule
+    rule_id = Column(Integer, ForeignKey(Rule.id), nullable=False, unique=True)
+    rule = relationship(Rule, backref=backref("tracking_rule", uselist=False))
+
+    name = Column(String(length=255), nullable=False)
+    params = Column(JSON)
