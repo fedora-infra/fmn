3313da928549fa7bec8d4d5e6d14ed00f8744957
Ryan Lerch <rlerch@redhat.com>
2022-10-13 17:33:23 +1000

    Query FASJSON for a list of users to track
    Previously, get_users() returned users who happened to be in the
    database (which is of little use). Now it acts as a proxy between the
    frontend and the FASJSON instance.
    
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- frontend/src/components/rule-edit/tracking-rule/ArtifactsOwnedParams.vue
+++ frontend/src/components/rule-edit/tracking-rule/ArtifactsOwnedParams.vue
@@ -1,45 +1,15 @@
 <script setup lang="ts">
-import { apiGet } from "@/api";
-import type { User } from "@/api/types";
 import { useUserStore } from "@/stores/user";
-import type { QueryFunction } from "react-query/types/core";
-import { ref } from "vue";
-import ArtifactsOwnedSummary from "./ArtifactsOwnedSummary.vue";
+import UserMultiSelect from "./UserMultiSelect.vue";
 
 const userStore = useUserStore();
 const username = userStore.username;
-const value = ref<string[]>(username ? [username] : []);
-const apiGetUsers = apiGet as QueryFunction<{ users: User[] }>;
-const route = "/users/";
-const getUsers = async (query: string) => {
-  if (!query) {
-    return value.value;
-  }
-  const results = await apiGetUsers({
-    queryKey: [route, { search: query }],
-    meta: undefined,
-  });
-  return results ? results.users.map((u) => u.name) : [];
-};
 </script>
 
 <template>
-  <FormKit
-    type="multiselect"
-    name="params"
+  <UserMultiSelect
+    :initialvalue="username ? [username] : []"
     label="Artifacts owned by:"
-    label-class="fw-bold"
-    mode="tags"
-    v-model="value"
-    :msOptions="getUsers"
-    :close-on-select="false"
-    searchable
-    :filter-results="false"
-    validation="required"
-    :min-chars="3"
-    :delay="0"
+    showArtifactsOwnedSummary
   />
-  <ArtifactsOwnedSummary :users="value" />
 </template>
-
-<style src="@vueform/multiselect/themes/default.css"></style>
--- frontend/src/components/rule-edit/tracking-rule/UsersFollowedParams.vue
+++ frontend/src/components/rule-edit/tracking-rule/UsersFollowedParams.vue
@@ -1,39 +1,11 @@
 <script setup lang="ts">
-import { apiGet } from "@/api";
-import type { User } from "@/api/types";
-import type { QueryFunction } from "react-query/types/core";
-import { ref } from "vue";
-
-const value = ref<string[]>([]);
-const apiGetUsers = apiGet as QueryFunction<{ users: User[] }>;
-const route = "/users/";
-const getUsers = async (query: string) => {
-  const results = await apiGetUsers({
-    queryKey: [route, { search: query }],
-    meta: undefined,
-  });
-  return results ? results.users.map((u) => u.name) : [];
-};
+import UserMultiSelect from "./UserMultiSelect.vue";
 </script>
 
 <template>
-  <FormKit
-    type="multiselect"
-    name="params"
-    label="Users:"
-    label-class="fw-bold"
-    placeholder="Search for users by username"
-    mode="tags"
-    v-model="value"
-    :msOptions="getUsers"
-    :close-on-select="false"
-    searchable
-    :filter-results="false"
-    :resolve-on-load="false"
-    validation="required"
-    :min-chars="3"
-    :delay="0"
+  <UserMultiSelect
+    :initialvalue="[]"
+    label="Users Pants:"
+    placeholder="Search for users by username pants"
   />
 </template>
-
-<style src="@vueform/multiselect/themes/default.css"></style>
