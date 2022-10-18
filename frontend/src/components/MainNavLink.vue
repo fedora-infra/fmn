7d5e6534f0a7b07de954cc4584b1ba552169cba3
Aurélien Bompard <aurelien@bompard.org>
2022-10-18 11:54:35 +0200

    Don't use fallthrough attributes in the newer Volar
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- frontend/src/components/MainNavLink.vue
+++ frontend/src/components/MainNavLink.vue
@@ -1,11 +1,12 @@
 <template>
   <li class="nav-item">
-    <router-link v-bind="$attrs" class="nav-link"><slot /></router-link>
+    <router-link v-bind="props" class="nav-link"><slot /></router-link>
   </li>
 </template>
 
-<script lang="ts">
-export default {
-  inheritAttrs: false,
-};
+<script setup lang="ts">
+import type { RouterLinkProps } from "vue-router";
+// The defineProps extractor can't currently use complex types such as RouterLinkProps directly
+// https://github.com/wheatjs/vite-plugin-vue-type-imports/issues/28
+const props = defineProps<{ to: RouterLinkProps["to"] }>();
 </script>
