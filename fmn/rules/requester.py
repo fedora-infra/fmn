5882771fc367495aef823d62b37d3c3130a437c8
Aurélien Bompard <aurelien@bompard.org>
2022-11-14 15:57:28 +0100

    Use a factory as default in handle_http_error
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- fmn/rules/requester.py
+++ fmn/rules/requester.py
@@ -14,7 +14,7 @@ if TYPE_CHECKING:
 log = logging.getLogger(__name__)
 
 
-def handle_http_error(default):
+def handle_http_error(default_factory):
     def exception_handler(f):
         @wraps(f)
         def wrapper(*args, **kw):
@@ -22,7 +22,7 @@ def handle_http_error(default):
                 return f(*args, **kw)
             except requests.HTTPError as e:
                 log.warning(f"Request failed: {e}")
-                return default
+                return default_factory()
 
         return wrapper
 
@@ -42,57 +42,57 @@ class Requester:
         self.distgit_client = DistGitService(self.urls["distgit"])
         self.fasjson_client = FasjsonService(self.urls["fasjson"])
 
-    @handle_http_error([])
+    @handle_http_error(list)
     def get_owned_by_user(self, artifact_type, username):
         log.debug(f"Getting owned {artifact_type} by {username}")
         return self.distgit_client.get_owned(artifact_type, username, "user")
 
-    @handle_http_error([])
+    @handle_http_error(list)
     def get_owned_by_group(self, artifact_type, groupname):
         log.debug(f"Getting owned {artifact_type} by group {groupname}")
         return self.distgit_client.get_owned(artifact_type, groupname, "group")
 
-    @handle_http_error([])
+    @handle_http_error(list)
     def get_package_owners(self, name: str):
         log.debug(f"Getting owners of rpms/{name}")
         return self.distgit_client.get_owners("rpms", name, "user")
 
-    @handle_http_error([])
+    @handle_http_error(list)
     def get_container_owners(self, name: str):
         log.debug(f"Getting owners of containers/{name}")
         return self.distgit_client.get_owners("containers", name, "user")
 
-    @handle_http_error([])
+    @handle_http_error(list)
     def get_module_owners(self, name: str):
         log.debug(f"Getting owners of modules/{name}")
         return self.distgit_client.get_owners("modules", name, "user")
 
-    @handle_http_error([])
+    @handle_http_error(list)
     def get_flatpak_owners(self, name: str):
         log.debug(f"Getting owners of flatpaks/{name}")
         return self.distgit_client.get_owners("flatpaks", name, "user")
 
-    @handle_http_error([])
+    @handle_http_error(list)
     def get_package_group_owners(self, name: str):
         log.debug(f"Getting group owners of rpms/{name}")
         return self.distgit_client.get_owners("rpms", name, "group")
 
-    @handle_http_error([])
+    @handle_http_error(list)
     def get_container_group_owners(self, name: str):
         log.debug(f"Getting group owners of containers/{name}")
         return self.distgit_client.get_owners("containers", name, "group")
 
-    @handle_http_error([])
+    @handle_http_error(list)
     def get_module_group_owners(self, name: str):
         log.debug(f"Getting group owners of modules/{name}")
         return self.distgit_client.get_owners("modules", name, "group")
 
-    @handle_http_error([])
+    @handle_http_error(list)
     def get_flatpak_group_owners(self, name: str):
         log.debug(f"Getting group owners of flatpaks/{name}")
         return self.distgit_client.get_owners("flatpaks", name, "group")
 
-    @handle_http_error([])
+    @handle_http_error(list)
     def get_user_groups(self, name: str):
         log.debug(f"Getting groups of user {name}")
         return self.fasjson_client.get_user_groups(name)
