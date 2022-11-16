7e4ce126d65779ea67a9a195b6af05e44baf7d8e
Nils Philippsen <nils@redhat.com>
2022-11-16 16:43:09 +0100

    Refactor FASJSONAsyncProxy
    How it was used in request handlers wasn’t tested, therefore they used
    the class improperly. This separates simply calling an endpoint and
    "unpaginating" better and implements the latter as an (async) iterator.
    
    Fixes: #656
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- tests/api/test_fasjson.py
+++ tests/api/test_fasjson.py
@@ -6,6 +6,13 @@ import pytest
 from fmn.api import fasjson
 
 
+@pytest.fixture
+def proxy(fasjson_url):
+    proxy = fasjson.FASJSONAsyncProxy(fasjson_url)
+    proxy.client = mock.AsyncMock()
+    return proxy
+
+
 class TestFASJSONAsyncProxy:
     PAGINATE_TOTAL_PAGES = 5
     PAGINATE_PER_PAGE = 5
@@ -42,52 +49,74 @@ class TestFASJSONAsyncProxy:
 
         return responses
 
-    @pytest.mark.parametrize("testcase", ("single", "paginated", "broken"))
-    async def test_get(self, testcase):
-        fasjson_url = "http://fasjson.test"
-        proxy = fasjson.FASJSONAsyncProxy(fasjson_url)
-        proxy.client = client = mock.AsyncMock()
+    async def test_get(self, proxy):
+        proxy.client.get.return_value = response = mock.Mock()
+        response.json.return_value = sentinel = object()
 
+        result = await proxy.get("url", foo="bar")
+
+        proxy.client.get.assert_awaited_once_with("url", foo="bar")
+        response.raise_for_status.assert_called_once_with()
+        assert result is sentinel
+
+    @mock.patch("fmn.api.fasjson.FASJSONAsyncProxy.get")
+    async def test_get_result(self, proxy_get, proxy):
+        sentinel = object()
+        proxy_get.return_value = {"result": sentinel}
+
+        result = await proxy.get_result("boo")
+
+        proxy_get.assert_awaited_once_with("boo")
+        assert result is sentinel
+
+    @pytest.mark.parametrize("testcase", ("success", "failure-missing-pagination"))
+    async def test_get_paginated(self, testcase, proxy):
         expectation = nullcontext()
 
-        if "single" in testcase:
-            response = mock.Mock()
-            response.json.return_value = {"result": {"boop": "yes"}}
-            client.get.return_value = response
-        elif "paginated" in testcase:
-            client.get.side_effect = self.get_paginated_responses()
-        elif "broken" in testcase:
-            client.get.side_effect = self.get_paginated_responses(broken=True)
-            expectation = pytest.raises(RuntimeError)
+        if "success" in testcase:
+            proxy.client.get.side_effect = self.get_paginated_responses()
+        else:
+            proxy.client.get.side_effect = self.get_paginated_responses(broken=True)
+            expectation = pytest.raises(ValueError)
 
         with expectation:
-            result = await proxy.get("/foo")
+            result = [x async for x in proxy.get_paginated("/foo")]
 
-        if "single" in testcase:
-            assert result == {"boop": "yes"}
-        elif "paginated" in testcase:
+        if "success" in testcase:
             assert isinstance(result, list)
             assert len(result) == self.PAGINATE_TOTAL_PAGES * self.PAGINATE_PER_PAGE
             assert all(i == item["boop"] for i, item in enumerate(result))
 
     @pytest.mark.parametrize(
-        "method, processed_kwargs, passed_through_kwargs, expected_path",
+        "method, kwargs, params, expected_path, is_iterator",
         (
-            ("search_users", {}, {"username": "foo"}, "/search/users/"),
-            ("get_user", {"username": "boop"}, {}, "/users/boop/"),
-            ("list_user_groups", {"username": "boop"}, {}, "/users/boop/groups/"),
+            ("search_users", {}, {"username": "foo"}, "/search/users/", True),
+            ("get_user", {"username": "boop"}, {}, "/users/boop/", False),
+            ("get_user_groups", {"username": "boop"}, {}, "/users/boop/groups/", False),
         ),
     )
-    def test_wrapper_method(self, method, processed_kwargs, passed_through_kwargs, expected_path):
+    async def test_wrapper_method(self, method, kwargs, params, expected_path, is_iterator):
         fasjson_url = "http://fasjson.test"
         proxy = fasjson.FASJSONAsyncProxy(fasjson_url)
-        proxy.get = mock.Mock()
-        proxy.get.return_value = sentinel = object()
+        sentinel = object()
+        if is_iterator:
+            proxy.get_paginated = mock.MagicMock()
+            proxy.get_paginated.return_value.__aiter__.return_value = [sentinel]
+        else:
+            proxy.get_result = mock.AsyncMock()
+            proxy.get_result.return_value = sentinel
 
-        retval = getattr(proxy, method)(**(processed_kwargs | passed_through_kwargs))
+        passed_through_kwargs = {"params": params} if params else {}
 
-        assert retval is sentinel
-        proxy.get.assert_called_once_with(expected_path, **passed_through_kwargs)
+        coro = getattr(proxy, method)(**(kwargs | params))
+        if is_iterator:
+            retval = [x async for x in coro]
+            assert retval == [sentinel]
+            proxy.get_paginated.assert_called_once_with(expected_path, **passed_through_kwargs)
+        else:
+            retval = await coro
+            assert retval is sentinel
+            proxy.get_result.assert_called_once_with(expected_path, **passed_through_kwargs)
 
 
 def test_get_fasjson_proxy():
