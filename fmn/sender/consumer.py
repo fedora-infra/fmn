a954c4aee2081ba1cfda0d643f8d01c1911f4b89
Aurélien Bompard <aurelien@bompard.org>
2022-11-22 13:23:17 +0100

    Fix how SSL is setup in the sender component
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- fmn/sender/consumer.py
+++ fmn/sender/consumer.py
@@ -4,6 +4,7 @@ import ssl
 
 from aio_pika import connect_robust
 from aio_pika.abc import SSLOptions
+from aio_pika.connection import URL
 
 _log = logging.getLogger(__name__)
 
@@ -13,27 +14,28 @@ CLOSING = object()
 
 class Consumer:
     def __init__(self, config, handler):
-        self._url = config["amqp_url"]
+        self._url = URL(config["amqp_url"])
         self._destination = config["queue"]
         self._handler = handler
+        self._url = self._url.update_query(
+            {"connection_name": f"FMN sender on {self._destination}"}
+        )
         if "tls" in config:
-            self._ssl_options = SSLOptions(
-                cafile=config["tls"]["ca_cert"],
-                certfile=config["tls"]["certfile"],
-                keyfile=config["tls"]["keyfile"],
-                no_verify_ssl=ssl.CERT_REQUIRED,
+            self._url = self._url.update_query(
+                SSLOptions(
+                    cafile=config["tls"]["ca_cert"],
+                    certfile=config["tls"]["certfile"],
+                    keyfile=config["tls"]["keyfile"],
+                    no_verify_ssl=ssl.CERT_REQUIRED,
+                )
             )
-        else:
-            self._ssl_options = None
         self._connection = None
         self._channel = None
         self._queue = None
         self._queue_iter = None
 
     async def connect(self):
-        self._connection = await connect_robust(
-            self._url, ssl_options=self._ssl_options, client_properties={"connection_name": "FMN"}
-        )
+        self._connection = await connect_robust(self._url)
         self._channel = await self._connection.channel()
         self._queue = await self._channel.declare_queue(
             self._destination, durable=True, auto_delete=False, exclusive=False
