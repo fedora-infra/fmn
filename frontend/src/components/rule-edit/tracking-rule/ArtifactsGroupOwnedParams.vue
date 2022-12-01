54b11f571922b214a196bcfe2f1a072be27f248c
Ryan Lerch <rlerch@redhat.com>
2022-12-01 13:49:55 +1000

    use the multiselect to populate the groups list in the UI
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- frontend/src/components/rule-edit/tracking-rule/ArtifactsGroupOwnedParams.vue
+++ frontend/src/components/rule-edit/tracking-rule/ArtifactsGroupOwnedParams.vue
@@ -3,46 +3,38 @@ import { apiGet } from "@/api";
 import type { Group } from "@/api/types";
 import { useUserStore } from "@/stores/user";
 import type { QueryFunction } from "react-query/types/core";
-import { onUnmounted, ref } from "vue";
-import { useQuery, useQueryClient } from "vue-query";
+import { ref } from "vue";
 import ArtifactsOwnedSummary from "./ArtifactsOwnedSummary.vue";
 
 const userStore = useUserStore();
 const value = ref<string[]>([]);
 const apiGetUserGroups = apiGet as QueryFunction<Group[]>;
 const url = `/api/v1/users/${userStore.username}/groups`;
-const { isLoading, isError, data, error } = useQuery(url, apiGetUserGroups);
-onUnmounted(async () => {
-  const client = useQueryClient();
-  await client.cancelQueries([url]);
-});
+
+const getUserGroups = async () => {
+  const results = await apiGetUserGroups({
+    queryKey: [url],
+    meta: undefined,
+  });
+  return results;
+};
 </script>
 
 <template>
-  <p v-if="isLoading">
-    <input
-      type="text"
-      class="form-control"
-      disabled
-      value="Loading groups..."
-    />
-  </p>
-  <span v-else-if="isError">Could not load groups: {{ error }}</span>
-  <template v-else>
-    <FormKit
-      type="multiselect"
-      name="params"
-      label="Artifacts owned by groups:"
-      label-class="fw-bold"
-      mode="tags"
-      v-model="value"
-      :msOptions="data || []"
-      searchable
-      :close-on-select="false"
-      validation="required"
-    />
-    <ArtifactsOwnedSummary :groups="value" />
-  </template>
+  <FormKit
+    type="multiselect"
+    name="params"
+    label="Artifacts owned by groups:"
+    label-class="fw-bold"
+    mode="tags"
+    v-model="value"
+    :msOptions="getUserGroups"
+    :close-on-select="false"
+    searchable
+    :filter-results="false"
+    :resolve-on-load="true"
+  />
+  <ArtifactsOwnedSummary :groups="value" />
 </template>
 
 <style src="@vueform/multiselect/themes/default.css"></style>
