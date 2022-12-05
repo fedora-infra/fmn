2480f0182d7ec5b59b8d30ea5c91c03719280f7f
Aurélien Bompard <aurelien@bompard.org>
2022-12-05 16:41:52 +0100

    Add a CLI module to peek into the cache
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

---
+++ fmn/rules/cli.py
@@ -0,0 +1,32 @@
+from pprint import pprint
+
+import click
+
+from fmn.core.config import get_settings
+from fmn.database import init_sync_model, sync_session_maker
+
+from .cache import cache
+from .requester import Requester
+
+
+@click.group("cache")
+def cache_cmd():
+    """Work with the cache in FMN."""
+
+
+@cache_cmd.command("show-tracked")
+def show_tracked():
+    """Show the current tracked value."""
+    cache.configure()
+    init_sync_model()
+    db = sync_session_maker()
+    requester = Requester(get_settings().dict()["services"])
+    pprint(cache.get_tracked(db, requester))
+
+
+@cache_cmd.command("delete-tracked")
+def delete_tracked():
+    """Invalidate the current tracked value."""
+    cache.configure()
+    cache.invalidate_tracked()
+    print("Tracked cache invalidated.")
