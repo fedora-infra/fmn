7cb848cbae5a430369c6e277a1124115ecee8543
Ryan Lerch <rlerch@redhat.com>
2022-12-13 13:26:05 +1000

    Use vue-multiselect async loading on applications chooser
    Resolves: #692
    
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- frontend/src/components/rule-edit/generation-rule/FilterApplication.vue
+++ frontend/src/components/rule-edit/generation-rule/FilterApplication.vue
@@ -1,35 +1,29 @@
 <script setup lang="ts">
 import { apiGet } from "@/api";
 import type { QueryFunction } from "react-query/types/core";
-import { useQuery } from "vue-query";
 
 // const props = defineProps<{ value: string | null }>();
 
-const route = "/api/v1/applications";
-const { isLoading, isError, data, error } = useQuery(
-  route,
-  apiGet as QueryFunction<string[]>
-);
+const apiGetApplications = apiGet as QueryFunction<string[]>;
+const url = "/api/v1/applications";
+
+const getApplications = async () => {
+  const results = await apiGetApplications({
+    queryKey: [url],
+    meta: undefined,
+  });
+  return results;
+};
 </script>
 
 <template>
   <p class="mb-0 fw-bold">Applications:</p>
-  <p v-if="isLoading">
-    <input
-      type="text"
-      class="form-control"
-      disabled
-      value="Loading applications..."
-    />
-  </p>
-  <p v-else-if="isError">Error: {{ error }}</p>
   <FormKit
-    v-else
     type="multiselect"
     name="applications"
     placeholder="Choose one or more applications"
     mode="tags"
-    :msOptions="data"
+    :msOptions="getApplications"
     searchable
     :close-on-select="false"
   />
