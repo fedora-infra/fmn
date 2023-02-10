f6d25e452c9b7d9c85660af07602727a2eeffed7
Nils Philippsen <nils@redhat.com>
2023-02-10 13:56:21 +0100

    Improve handling cashews caches in testing
    This consolidates how cashews caching is tweaked for testing in the
    cashews_cache() fixture. By default, cashews caching will be configured
    for an in-memory cache and disabled in tests, this can be changed by
    decorating the test or encompassing class with the corresponding
    cashews_cache marker.
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- tests/rules/conftest.py
+++ tests/rules/conftest.py
@@ -1,5 +1,4 @@
 import pytest
-from cashews import cache
 
 from fmn.cache import configure_cache
 
@@ -7,7 +6,3 @@ from fmn.cache import configure_cache
 @pytest.fixture(autouse=True)
 async def configured_cache():
     configure_cache()
-    yield
-    await cache.clear()
-    await cache.close()
-    cache._get_backend_and_config.cache_clear()
