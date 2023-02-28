81d918151d30606ecde3f6fd8d7421c052b399e7
Aurélien Bompard <aurelien@bompard.org>
2023-02-28 13:06:42 +0100

    Make F401 marking a bit less annoying
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- fmn/backends/__init__.py
+++ fmn/backends/__init__.py
@@ -1,3 +1,3 @@
-from .datagrepper import DatagrepperAsyncProxy  # noqa: F401
-from .fasjson import FASJSONAsyncProxy  # noqa: F401
-from .pagure import PagureAsyncProxy, PagureRole  # noqa: F401
+from .datagrepper import DatagrepperAsyncProxy
+from .fasjson import FASJSONAsyncProxy
+from .pagure import PagureAsyncProxy, PagureRole
--- fmn/database/model/__init__.py
+++ fmn/database/model/__init__.py
@@ -1,7 +1,7 @@
-from .destination import Destination  # noqa: F401
-from .filter import Filter  # noqa: F401
-from .generated import Generated  # noqa: F401
-from .generation_rule import GenerationRule  # noqa: F401
-from .rule import Rule  # noqa: F401
-from .tracking_rule import TrackingRule  # noqa: F401
-from .user import User  # noqa: F401
+from .destination import Destination
+from .filter import Filter
+from .generated import Generated
+from .generation_rule import GenerationRule
+from .rule import Rule
+from .tracking_rule import TrackingRule
+from .user import User
