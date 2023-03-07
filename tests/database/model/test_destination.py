bfb64cbf0a9afe5854d80196b0c7f4128ced6045
Ryan Lerch <rlerch@redhat.com>
2023-03-07 12:28:52 +1000

    Add appname and url to notifications that we generate
    Resolves: #769
    
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- tests/database/model/test_destination.py
+++ tests/database/model/test_destination.py
@@ -6,32 +6,44 @@ from fmn.database.model import Destination
 def test_email(make_mocked_message):
     d = Destination(id=1, protocol="email", address="dummy@example.com")
     message = make_mocked_message(
-        topic="dummy", body={"summary": "dummy summary", "content": "dummy content"}
+        topic="dummy",
+        body={
+            "summary": "dummy summary",
+            "content": "dummy content",
+            "app": "dummy",
+            "url": "https://dummy.org/dummylink",
+        },
     )
     result = d.generate(message)
     assert result == {
-        "headers": {"To": "dummy@example.com", "Subject": "dummy summary"},
-        "body": "dummy content",
+        "headers": {"To": "dummy@example.com", "Subject": "[dummy] dummy summary"},
+        "body": "dummy content\nhttps://dummy.org/dummylink",
     }
 
 
 def test_irc(make_mocked_message):
     d = Destination(id=1, protocol="irc", address="dummy")
-    message = make_mocked_message(topic="dummy", body={"summary": "dummy summary"})
+    message = make_mocked_message(
+        topic="dummy",
+        body={"summary": "dummy summary", "app": "dummy", "url": "https://dummy.org/dummylink"},
+    )
     result = d.generate(message)
     assert result == {
         "to": "dummy",
-        "message": "dummy summary",
+        "message": "[dummy] dummy summary https://dummy.org/dummylink",
     }
 
 
 def test_matrix(make_mocked_message):
     d = Destination(id=1, protocol="matrix", address="@dummy:example.com")
-    message = make_mocked_message(topic="dummy", body={"summary": "dummy summary"})
+    message = make_mocked_message(
+        topic="dummy",
+        body={"summary": "dummy summary", "app": "dummy", "url": "https://dummy.org/dummylink"},
+    )
     result = d.generate(message)
     assert result == {
         "to": "@dummy:example.com",
-        "message": "dummy summary",
+        "message": "[dummy] dummy summary https://dummy.org/dummylink",
     }
 
 
