7186e9f7b8f9e17c8d405604398f27c1f66a60bd
Nils Philippsen <nils@redhat.com>
2023-03-31 14:26:48 +0200

    Straighten out API for querying artifacts
    This merges the `/artifacts/owned` endpoint into `/artifacts` and also
    shifts applying wildcards to name patterns from the backend to the
    frontend.
    
    Fixes: #779
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- tests/api/handlers/test_misc.py
+++ tests/api/handlers/test_misc.py
@@ -39,11 +39,8 @@ class TestMisc(BaseTestAPIV1Handler):
             "planet",
         ]
 
-    @pytest.mark.parametrize("ownertype", ("user", "group"))
-    async def test_get_projects(self, client, respx_mocker, ownertype):
-        settings = get_settings()
-        settings.services.distgit_url = "http://distgit.test"
-
+    @staticmethod
+    def mock_distgit_owned_projects(respx_mocker, settings, ownertype):
         if ownertype == "user":
             distgit_endpoint = f"{settings.services.distgit_url}/api/0/projects"
             params = {"fork": False, "short": False, "username": "dudemcpants"}
@@ -91,22 +88,87 @@ class TestMisc(BaseTestAPIV1Handler):
             ]
         )
 
-        if ownertype == "user":
-            response = client.get(f"{self.path}/artifacts/owned", params={"users": ["dudemcpants"]})
-        elif ownertype == "group":
-            response = client.get(f"{self.path}/artifacts/owned", params={"groups": ["dudegroup"]})
+        return route
 
-        assert route.called
+    @staticmethod
+    def mock_distgit_projects(respx_mocker, settings):
+        distgit_endpoint = f"{settings.services.distgit_url}/api/0/projects"
+        params = {"pattern": "*foobar*"}
+        distgit_json_response = {
+            "pagination": {
+                "pages": 1,
+            },
+            "projects": [
+                {
+                    "description": "foobar containers",
+                    "fullname": "containers/foobar",
+                    "name": "foobar",
+                    "namespace": "containers",
+                },
+                {
+                    "description": "foobar rpms",
+                    "fullname": "rpms/foobar",
+                    "name": "foobar",
+                    "namespace": "rpms",
+                },
+                # Some garbage for the handler to cope with:
+                {
+                    "description": "Hahahhaha!!!",
+                    "fullname": "i-don’t-exist/hahaha",
+                    "name": "hahaha",
+                    "namespace": "i-don’t-exist",
+                    "access_users": {"admin": ["dudemcpants"]},
+                },
+            ],
+        }
 
-        assert response.json() == [
-            {"name": "pants", "type": "containers"},
-            {"name": "trousers", "type": "rpms"},
-        ]
+        route = respx_mocker.get(distgit_endpoint, params=params).mock(
+            side_effect=[
+                Response(
+                    status.HTTP_200_OK,
+                    json=distgit_json_response,
+                )
+            ]
+        )
 
-    def test_get_projects_no_user_or_group(self, client):
-        response = client.get(f"{self.path}/artifacts/owned")
-        assert response.status_code == status.HTTP_200_OK
-        assert response.json() == []
+        return route
+
+    @pytest.mark.parametrize("testcase", ("user", "group", "user-group", "name", "nothing"))
+    async def test_get_artifacts(self, client, respx_mocker, testcase):
+        settings = get_settings()
+        settings.services.distgit_url = "http://distgit.test"
+
+        routes = {}
+        query_params = {}
+
+        if "user" in testcase:
+            routes["user"] = self.mock_distgit_owned_projects(respx_mocker, settings, "user")
+            query_params["users"] = ["dudemcpants"]
+
+        if "group" in testcase:
+            routes["group"] = self.mock_distgit_owned_projects(respx_mocker, settings, "group")
+            query_params["groups"] = ["dudegroup"]
+
+        if "name" in testcase:
+            routes["name"] = self.mock_distgit_projects(respx_mocker, settings)
+            query_params["names"] = ["*foobar*"]
+
+        response = client.get(f"{self.path}/artifacts", params=query_params)
+        result = response.json()
+
+        for what in ("user", "group", "name"):
+            if what in testcase:
+                assert routes[what].called
+
+        if "user" in testcase or "group" in testcase:
+            assert {"name": "pants", "type": "containers"} in result
+            assert {"name": "trousers", "type": "rpms"} in result
+
+        if "name" in testcase:
+            assert {"name": "foobar", "type": "containers"} in result
+            assert {"name": "foobar", "type": "rpms"} in result
+
+        assert result == sorted(result, key=lambda item: (item["type"], item["name"]))
 
     def test_liveness(self, client):
         response = client.get(f"{self.path}/healthz/live")
@@ -139,54 +201,3 @@ class TestMisc(BaseTestAPIV1Handler):
         response = client.get(f"{self.path}/healthz/ready")
         assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
         assert response.json()["detail"] == "Database schema needs to be upgraded"
-
-    async def test_get_artifacts(self, client, respx_mocker):
-        settings = get_settings()
-        settings.services.distgit_url = "http://distgit.test"
-
-        distgit_endpoint = f"{settings.services.distgit_url}/api/0/projects"
-        params = {"pattern": "*foobar*"}
-        distgit_json_response = {
-            "pagination": {
-                "pages": 1,
-            },
-            "projects": [
-                {
-                    "description": "foobar containers",
-                    "fullname": "containers/foobar",
-                    "name": "foobar",
-                    "namespace": "containers",
-                },
-                {
-                    "description": "foobar rpms",
-                    "fullname": "rpms/foobar",
-                    "name": "foobar",
-                    "namespace": "rpms",
-                },
-                # Some garbage for the handler to cope with:
-                {
-                    "description": "Hahahhaha!!!",
-                    "fullname": "i-don’t-exist/hahaha",
-                    "name": "hahaha",
-                    "namespace": "i-don’t-exist",
-                },
-            ],
-        }
-
-        route = respx_mocker.get(distgit_endpoint, params=params).mock(
-            side_effect=[
-                Response(
-                    status.HTTP_200_OK,
-                    json=distgit_json_response,
-                )
-            ]
-        )
-
-        response = client.get(f"{self.path}/artifacts", params={"name": ["foobar"]})
-
-        assert route.called
-
-        assert response.json() == [
-            {"name": "foobar", "type": "containers"},
-            {"name": "foobar", "type": "rpms"},
-        ]
