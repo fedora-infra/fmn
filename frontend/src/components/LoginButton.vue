91f19b4c363067c7862b902e9d94eb1ab236fd71
Aurélien Bompard <aurelien@bompard.org>
2023-04-06 18:46:29 +0200

    Don't show the login button on the OIDC callback page
    Also, show a spinner when clicked.
    
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- frontend/src/components/LoginButton.vue
+++ frontend/src/components/LoginButton.vue
@@ -5,19 +5,27 @@ SPDX-License-Identifier: MIT
 -->
 
 <template>
-  <CDropdown v-if="userStore.loggedIn" variant="nav-item" alignment="end">
-    <CDropdownToggle component="a">
-      <img :alt="userStore.user.username!" :src="avatarURL" />
-    </CDropdownToggle>
-    <CDropdownMenu>
-      <CDropdownItem @click="doLogout()">{{ t("logout") }}</CDropdownItem>
-    </CDropdownMenu>
-  </CDropdown>
-  <CNavItem v-else>
-    <CButton @click.prevent="doLogin()" component="a" color="primary">
-      {{ t("login") }}
-    </CButton>
-  </CNavItem>
+  <template v-if="showButton">
+    <CDropdown v-if="userStore.loggedIn" variant="nav-item" alignment="end">
+      <CDropdownToggle component="a">
+        <img :alt="userStore.user.username!" :src="avatarURL" />
+      </CDropdownToggle>
+      <CDropdownMenu>
+        <CDropdownItem @click="doLogout()">{{ t("logout") }}</CDropdownItem>
+      </CDropdownMenu>
+    </CDropdown>
+    <CNavItem v-else>
+      <CButton
+        @click.prevent="doLogin()"
+        component="a"
+        color="primary"
+        :disabled="loading"
+      >
+        {{ t("login") }}
+        <CSpinner v-if="loading" size="sm" class="ms-1" />
+      </CButton>
+    </CNavItem>
+  </template>
 </template>
 
 <script setup lang="ts">
@@ -28,16 +36,16 @@ import {
   CDropdownMenu,
   CDropdownToggle,
   CNavItem,
+  CSpinner,
 } from "@coreui/bootstrap-vue";
-import { computed } from "vue";
+import { computed, onUnmounted, ref } from "vue";
 import { useI18n } from "vue-i18n";
-import { useRoute, useRouter } from "vue-router";
+import { useRouter } from "vue-router";
 import { login, logout, useAuth } from "../auth";
 import { useUserStore } from "../stores/user";
 import { generateLibravatarURL } from "../util";
 
 const auth = useAuth();
-const route = useRoute();
 const userStore = useUserStore();
 const router = useRouter();
 const { t } = useI18n();
@@ -46,14 +54,32 @@ const avatarURL = computed(() =>
   generateLibravatarURL(userStore.email, 30, "retro")
 );
 
-const doLogin = () => login(auth, route.fullPath);
+const loading = ref(false);
+const loadingTimer = ref<number | null>(null);
+
+const doLogin = async () => {
+  loading.value = true;
+  await login(auth, router.currentRoute.value.fullPath);
+  loadingTimer.value = window.setTimeout(() => {
+    loading.value = false;
+  }, 3000);
+};
 
 const doLogout = () => {
   logout();
-  if (route.meta.auth) {
+  if (router.currentRoute.value.meta.auth) {
     router.push("/");
   }
 };
+
+onUnmounted(() => {
+  loadingTimer.value && window.clearTimeout(loadingTimer.value);
+});
+
+const showButton = computed(
+  // Don't show the button on the OIDC callback
+  () => router.currentRoute.value.path !== "/login/fedora"
+);
 </script>
 
 <i18n lang="yaml">
