7b64ba0afe0b5b4c1687bdb4324f7b23d4afad1e
Aurélien Bompard <aurelien@bompard.org>
2023-04-25 17:55:18 +0200

    Message schema: make rule.id and user.name required as well
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- tests/messages/test_base.py
+++ tests/messages/test_base.py
@@ -3,20 +3,33 @@
 # SPDX-License-Identifier: MIT
 
 import pytest
-from fedora_messaging.message import ValidationError
+from jsonschema.exceptions import ValidationError
 
 from fmn.messages.rule import RuleCreateV1
 
 
 def test_messages_base():
-    message = RuleCreateV1({"rule": {}, "user": {}})
+    message = RuleCreateV1({"rule": {"id": 1}, "user": {"name": "dummy"}})
     assert message.app_name == "FMN"
     assert message.app_icon == "https://apps.fedoraproject.org/img/icons/fedmsg.png"
-
-
-def test_messages_rule_no_name():
-    message = RuleCreateV1({"rule": {"name": None}, "user": {}})
     try:
         message.validate()
     except ValidationError as e:
         pytest.fail(e)
+
+
+def test_messages_rule_no_name():
+    message = RuleCreateV1({"rule": {"id": 1, "name": None}, "user": {"name": "dummy"}})
+    try:
+        message.validate()
+    except ValidationError as e:
+        pytest.fail(e)
+
+
+@pytest.mark.parametrize(
+    "body", [{"rule": {}, "user": {"name": "dummy"}}, {"rule": {"id": 1}, "user": {}}]
+)
+def test_messages_rule_missing_values(body):
+    message = RuleCreateV1(body)
+    with pytest.raises(ValidationError):
+        message.validate()
