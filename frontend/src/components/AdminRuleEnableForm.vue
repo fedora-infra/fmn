d4c51e7958ed0e41d3ff4ff076b373a95aa4fa4b
Aurélien Bompard <aurelien@bompard.org>
2023-11-08 12:04:54 +0100

    Adjust to some FormKit changes
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- frontend/src/components/AdminRuleEnableForm.vue
+++ frontend/src/components/AdminRuleEnableForm.vue
@@ -66,7 +66,7 @@ const handleSubmit = async (
     id="rule"
     @submit="handleSubmit"
     :actions="false"
-    #default="{ disabled: formDisabled }"
+    #default="{ state: { disabled: formDisabled } }"
   >
     <FormKit type="hidden" name="id" :value="props.rule.id.toString()" />
     <FormKit
