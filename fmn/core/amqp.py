704d09ac9031ef743fcdb683104a6f0be95a515a
Aurélien Bompard <aurelien@bompard.org>
2024-10-28 15:16:19 +0100

    Move the new function to `fmn.database` to preserve the dependencies split
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- fmn/core/amqp.py
+++ fmn/core/amqp.py
@@ -4,11 +4,9 @@
 
 import logging
 import ssl
-from datetime import datetime, timezone
 
 from aio_pika.abc import SSLOptions
 from aio_pika.connection import URL
-from fedora_messaging.message import Message
 
 log = logging.getLogger(__name__)
 
@@ -26,16 +24,3 @@ def get_url_from_config(config: dict):
             )
         )
     return url
-
-
-def get_sent_datetime(message: Message) -> datetime:
-    sent_at = message._headers.get("sent-at", None)
-    if sent_at:
-        # fromisoformat doesn't parse Z suffix (yet) see:
-        # https://discuss.python.org/t/parse-z-timezone-suffix-in-datetime/2220
-        try:
-            return datetime.fromisoformat(sent_at.replace("Z", "+00:00"))
-        except ValueError:
-            log.exception("Failed to parse sent-at timestamp value")
-    # Default to now
-    return datetime.now(tz=timezone.utc)
--- tests/core/test_amqp.py
+++ tests/core/test_amqp.py
@@ -3,12 +3,10 @@
 # SPDX-License-Identifier: MIT
 
 import ssl
-from datetime import datetime, timezone
 
-import pytest
 from aio_pika.connection import URL
 
-from fmn.core.amqp import get_sent_datetime, get_url_from_config
+from fmn.core.amqp import get_url_from_config
 
 
 async def test_get_url_from_config_with_ssl(mocker):
@@ -30,27 +28,3 @@ async def test_get_url_from_config_with_ssl(mocker):
         }
     )
     assert get_url_from_config(config) == expected
-
-
-FAKE_NOW = datetime(2021, 1, 1, 0, 0, 0, tzinfo=timezone.utc)
-
-
-@pytest.mark.parametrize(
-    "sent_at,expected",
-    [
-        (None, FAKE_NOW),
-        ("2021-07-27T04:22:42Z", datetime(2021, 7, 27, 4, 22, 42, tzinfo=timezone.utc)),
-        ("2021-07-27T04:22:42JUNK", FAKE_NOW),
-    ],
-)
-def test_get_sent_datetime(make_mocked_message, mocker, sent_at, expected):
-    message = make_mocked_message(
-        topic="dummy",
-        body={"summary": "dummy summary"},
-    )
-    message._properties.headers["sent-at"] = sent_at
-
-    fake_datetime = mocker.patch("fmn.core.amqp.datetime")
-    fake_datetime.now.return_value = FAKE_NOW
-    fake_datetime.fromisoformat = datetime.fromisoformat
-    assert get_sent_datetime(message) == expected
