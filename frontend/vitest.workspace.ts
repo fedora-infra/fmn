583b5af481d153b2a00d4ca730e101e39e928c67
Aurélien Bompard <aurelien@bompard.org>
2025-11-10 12:11:02 +0100

    Remove the workspace config file for vite, it's deprecated
    See: https://vitest.dev/guide/projects
    
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- frontend/vitest.workspace.ts
+++
@@ -1,7 +0,0 @@
-// SPDX-FileCopyrightText: Contributors to the Fedora Project
-//
-// SPDX-License-Identifier: MIT
-
-import { defineWorkspace } from "vitest/config";
-
-export default defineWorkspace(["./vite.config.ts"]);
